7d4be3b64d6109b481b989e797cd046df38abd5b
Hangfei Lin <1023584582@qq.com>
2024-06-21 17:26:20 -0400

    sendImage and sendVideo mutations added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { hostname: "utmost-shrimp-161.convex.cloud" },
+    //   { hostname: "oaidalleapiprodscus.blob.core.windows.net" },
+    ],
+  },
+};
 
 export default nextConfig;
